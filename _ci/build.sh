a29d1da51fc20f86c6d8a4c3e77a7e10adb5ba93
tzclucian <tzclucian@gmail.com>
2019-11-22 12:06:29 +0200

    GOOGLEDOCS_464 : Update to build on Travis-CI (rather than Bamboo)    - refactored the modules

--- _ci/build.sh
+++ _ci/build.sh
@@ -9,7 +9,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 # If the branch is "master" and the commit is not a Pull Request then deploy the JAR SNAPSHOT artifacts
 [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ] && DEPLOY="deploy" || DEPLOY="install"
 
-mvn -B -U -Dbuildnumber=${TRAVIS_BUILD_NUMBER} clean ${DEPLOY} -Pcommunity
+mvn -B -U -Dbuildnumber=${TRAVIS_BUILD_NUMBER} clean ${DEPLOY}
 
 
 popd
