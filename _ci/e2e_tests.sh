62ca8606f7b5178b7cbd4dae6514bfe86e65111a
DenisGabriela <Denis.Ungureanu1@ness.com>
2019-12-02 15:59:38 +0200

    GOOGLEDOCS-468 : Setup End-to-End Docker test environment    - add configurations to run end2end tests on Travis build

---
+++ _ci/e2e_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting End-to-End Tests Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+
+mvn -B -U clean install -Plocal \
+ -DbuildNumber=${TRAVIS_BUILD_NUMBER} \
+ -DskipTests
+
+mvn -B -U clean verify -Pdocker-end-to-end-setup -pl 'alfresco-googledrive-end-to-end-tests'
+
+popd
+set +vex
+echo "=========================== Finishing End-to-End Tests Script =========================="
