a3633471259562a3dab0a8db37834991f2623567
Cezar.Leahu <cezar.leahu@ness.com>
2019-12-04 13:45:19 +0200

    GOOGLEDOCS-468 : Setup End-to-End Docker test environment
    - set specific maven plugin versions
    - update license
    - POM formatting

--- alfresco-googledrive-end-to-end-tests/src/test/java/org/alfresco/integrations/google/docs/GoogleDocsIT.java
+++ alfresco-googledrive-end-to-end-tests/src/test/java/org/alfresco/integrations/google/docs/GoogleDocsIT.java
@@ -1,9 +1,16 @@
-/*
- * Copyright 2015-2018 Alfresco Software, Ltd.  All rights reserved.
+/**
+ * Copyright (C) 2005-2019 Alfresco Software Limited.
  *
- * License rights for this program may be obtained from Alfresco Software, Ltd.
- * pursuant to a written agreement and any use of this program without such an
- * agreement is prohibited.
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Alfresco. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 package org.alfresco.integrations.google.docs;
