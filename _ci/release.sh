9177eeede57707c957c9e60fa68ea609369850fd
kshah <kavit.shah@globallogic.com>
2023-01-14 13:46:26 +0530

    Google Docs 3.4.0-A2 [trigger release]

--- _ci/release.sh
+++ _ci/release.sh
@@ -5,8 +5,8 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-# For PR builds only execute a Dry Run of the release
-[ "$GITHUB_EVENT" = "pull_request" ] && DRY_RUN="" || DRY_RUN="-DdryRun"
+## For PR builds only execute a Dry Run of the release
+#[ "$GITHUB_EVENT" = "pull_request" ] && DRY_RUN="" || DRY_RUN="-DdryRun"
 
 # Travis CI runner work on DETACHED HEAD, so we need to checkout the release branch
 git checkout -B "${BRANCH_NAME}"
@@ -16,7 +16,6 @@ git config user.name "${GIT_USERNAME}"
 
 # Run the release plugin - with "[skip ci]" in the release commit message
 mvn -B \
-    ${DRY_RUN} \
     "-Darguments=-DskipTests -DbuildNumber=$GITHUB_RUN_NUMBER" \
     release:clean release:prepare release:perform \
     -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
