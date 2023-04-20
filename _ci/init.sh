00c3f7bafd10f1edd771065e4ba41fcec2fa903b
kshah <kavit.shah@globallogic.com>
2023-04-20 14:43:03 +0530

    Upgrade Repo and Share versions

--- _ci/init.sh
+++ _ci/init.sh
@@ -6,12 +6,15 @@ set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 mkdir -p ${HOME}/.m2 && cp -rf _ci/settings.xml ${HOME}/.m2/
-echo "${QUAY_PASSWORD}" | docker login -u="alfresco+travis_ci" --password-stdin quay.io
+# Docker Logins
+echo "${DOCKERHUB_PASSWORD}" | docker login -u="${DOCKERHUB_USERNAME}" --password-stdin
+echo "${QUAY_PASSWORD}" | docker login -u="${QUAY_USERNAME}" --password-stdin quay.io
+
 find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
 
-# Enable experimental docker features (e.g. squash options)
-echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
+## Enable experimental docker features (e.g. squash options)
+#echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+#sudo service docker restart
 
 popd
 set +vex
